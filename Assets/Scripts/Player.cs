b545b11fe5efac89aa7c9e0068ab2ea245c37fda
Yj621 <87031282+Yj621@users.noreply.github.com>
2024-01-28 23:49:07 +0900

    FEAT : Tag) Food and Bug that eat up no matter what size they are
    FEAT : Tag) Food, Bug) that eat up no matter what size they are

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour
 {
     public FloatingJoystick joystick; // FloatingJoystick 참조를 저장하기 위한 변수
     public float moveSpeed = 5f; // 플레이어의 이동 속도
+    public float growthRate = 0.1f; // 플레이어 성장 속도
 
     // Update is called once per frame
     void Update()
@@ -18,4 +19,24 @@ public class Player : MonoBehaviour
         Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput);
         transform.position += movement * moveSpeed * Time.deltaTime;
     }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Food") && other.transform.localScale.x < transform.localScale.x)
+        {
+            // 작은 물체만 흡수
+            AbsorbObject(other.gameObject);
+        }
+    }
+
+    void AbsorbObject(GameObject objectToAbsorb)
+    {
+        // 물체를 흡수하고 크기를 키움
+        transform.localScale += new Vector3(growthRate, growthRate, growthRate);
+
+        // 흡수한 물체 제거
+        Destroy(objectToAbsorb);
+    }
+
 }
